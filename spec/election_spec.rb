e9132291a0a48aa072656eabba524b3bccac92b9
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-14 14:39:23 -0400

    add race2 info and candidates

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Election do
   before(:each) do
     @election = Election.new("1999")
     @race1 = Race.new("Texas Governor")
+    @race2 = Race.new("North Dakota Mayor")
   end
 
   describe "#initialize" do
@@ -34,25 +35,34 @@ RSpec.describe Election do
   describe "add candidates" do
     it "can add candidates to candidate array" do
       @election.add_race(@race1)
-      candidate1 = @race1.register_candidate!({name: "Diana D", party: :democrat})
-      candidate2 = @race1.register_candidate!({name: "Roberto R", party: :republican})
-
-      expect(@election.candidates(@race1)).to eq([candidate1, candidate2])
+      @election.add_race(@race2)
+      @candidate1 = @race1.register_candidate!({name: "Diana D", party: :democrat})
+      @candidate2 = @race1.register_candidate!({name: "Roberto R", party: :republican})
+      @candidate3 = @race2.register_candidate!({name: "Bob B", party: :green})
+      @candidate4 = @race2.register_candidate!({name: "Mickey Mouse", party: :fun})
+      expect(@election.candidates(@race1)).to eq([@candidate1, @candidate2])
+      expect(@election.candidates(@race2)).to eq([@candidate3, @candidate4])
     end
   end
 
   describe "#vote counts" do
     it "adds candidates name and their vote counts to a hash" do
       @election.add_race(@race1)
-      candidate1 = @race1.register_candidate!({name: "Diana D", party: :democrat})
-      candidate2 = @race1.register_candidate!({name: "Roberto R", party: :republican})
-      candidate1.vote_for!
-      candidate1.vote_for!
-      candidate2.vote_for!
+      @election.add_race(@race2)
+      @candidate1 = @race1.register_candidate!({name: "Diana D", party: :democrat})
+      @candidate2 = @race1.register_candidate!({name: "Roberto R", party: :republican})
+      @candidate3 = @race2.register_candidate!({name: "Bob B", party: :green})
+      @candidate4 = @race2.register_candidate!({name: "Mickey Mouse", party: :fun})
+      6.times { @candidate1.vote_for! }
+      10.times { @candidate2.vote_for! }
+      16.times { @candidate3.vote_for! }
+      100.times { @candidate4.vote_for! }
       
       expect(@election.vote_counts(@race1)).to be_a(Hash)
-      expect(@election.vote_counts(@race1)).to eq({ "Diana D" => 2, 
-                                                    "Roberto R" => 1})
+      expect(@election.vote_counts(@race1)).to eq({ "Diana D" => 6, 
+                                                    "Roberto R" => 10})
+      expect(@election.vote_counts(@race2)).to eq({ "Bob B" => 16, 
+                                                    "Mickey Mouse" => 100})
     end
   end
 end
