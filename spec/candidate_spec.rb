afcd3b69d244309fb33577d235e904edd5e16c0c
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-14 11:11:17 -0400

    add instance variables to candidate class

--- spec/candidate_spec.rb
+++ spec/candidate_spec.rb
@@ -10,5 +10,19 @@ RSpec.describe Candidate do
       expect(@diana).to be_a(Candidate)
     end
 
+    it "has attributes" do
+      expect(@diana.name).to eq("Diana D")
+      expect(@diana.party).to eq(:democrat)
+    end
+
+    it "starts with 0 votes" do
+      expect(@diana.votes).to eq(0)
+    end
+  end
+
+  describe "#vote for" do
+    it "can count how many votes candidate has" do
+      
+    end
   end
 end
